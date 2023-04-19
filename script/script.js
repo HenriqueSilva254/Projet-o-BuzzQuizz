18fa09c5b1d43a0dfb9ef96ed0852da9d5706db3
luigipr <luigiprofeti@gmail.com>
2023-04-19 15:10:48 -0300

    Versão com o botão criar perguntas

---
+++ script/script.js
@@ -0,0 +1,10 @@
+
+
+function criarPerguntas() {
+    const criarQuizz = document.querySelector('.criarQuizz');
+    const criarPerguntas = document.querySelector('.criarPerguntas');
+
+    criarQuizz.classList.add('escondido')
+    criarPerguntas.classList.remove('escondido')
+
+}
